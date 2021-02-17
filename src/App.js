e1f653a39e6adbe538ac003659e96de153510192
Luis Lopez <lalelopez2003@utexas.edu>
2021-02-17 15:15:21 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, {useState, useRef} from "react";
+import "./styles/app.scss";
+import data from "./data";
+//Adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+
+function App() {
+  const audioRef = useRef(null);
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const [libraryStatus,setLibraryStatus] = useState(false);
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    const roundedDuration = Math.round(duration);
+    const roundedCurrent = Math.round(current);
+    const animation = Math.round((roundedCurrent / roundedDuration)*100);
+
+    setSongInfo({...songInfo,currentTime: current, duration, animationPercentage:animation});
+  };
+
+  const sonEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex +1) % songs.length]);
+    if(isPlaying) audioRef.current.play();
+  }
+
+  return (
+    <div className={`App ${libraryStatus ? 'library-active' : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
+
+      <Song currentSong={currentSong} />
+      <Player 
+        audioRef = {audioRef}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+        songInfo = {songInfo}
+        setSongInfo = {setSongInfo}
+        songs = {songs}
+        setCurrentSong = {setCurrentSong}
+        setSongs={setSongs}
+      />
+      <Library 
+        songs={songs} 
+        setCurrentSong={setCurrentSong}
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+        />
+      <audio 
+        onTimeUpdate={timeUpdateHandler} 
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef} 
+        src={currentSong.audio}
+        onEnded = {sonEndHandler}
+        ></audio>
+    </div>
+  );
+}
+
+export default App;
